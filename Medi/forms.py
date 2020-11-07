851ea458b4f78118dc16bf60f3a250ff1de915d7
juru-juliette <kangabejuliette@gmail.com>
2020-11-07 16:08:23 +0200

    creating database

--- Medi/forms.py
+++ Medi/forms.py
@@ -1,25 +1,7 @@
-from django.contrib.auth import get_user_model
-
+# from django.contrib.auth import get_user_model
+from django import forms
+from .models import *
 # check for unique email and username
-user = get_user_model()
-
-# class LoginForm(forms.Form):
-#     username =forms.charField()
-#     password =forms.charField(
-#         widget= forms.PasswordInput(
-#             attrs ={
-#                 "class": "form-control",
-#                 "id": "user-password"
-#             }
-#         )
-#     )
-
-# def clean_username(self):
-#     username =self.cleaned.data.get("username")
-#     qs = User.objects.filter(username__iexact =username_) 
-#     # thisIsMyUsername ==thisismyusername
-#     if not qs.exits():
-#         raise forms.ValidationError("this is an invalid user.
-#         ")
-        
-#     return username
+class ProfileForm(forms.ModelForm):
+    class Meta:
+         model= Profile
--- Medi/views.py
+++ Medi/views.py
@@ -8,5 +8,19 @@ from django.shortcuts import render
 def home(request):
     # return HttpResponse('Welcome to Hellomed')
     return render(request, 'index.html')
+    
+def profile(request, profile_id):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.username = current_user
+            profile.save()
+            return redirect('home')
 
+    else:
+        form = ProfileForm()
+    username=User.objects.all()    
+    myProfile = Profile.objects.filter(username = current_user)
     
