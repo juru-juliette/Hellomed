121774fe4fb4ebfb4650168207574b3bf6c79fa2
juru-juliette <kangabejuliette@gmail.com>
2020-11-12 16:48:23 +0200

    password changed

--- Hello_med/settings.py
+++ Hello_med/settings.py
@@ -137,6 +137,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'kangabejuliette@gmail.com'
-EMAIL_HOST_PASSWORD = 'juruyuly'
+EMAIL_HOST_PASSWORD = 'Juruyuly'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
